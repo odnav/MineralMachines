8b8f90eb00b06dc9a00762377625e565f4332cbf
odnav <vandopleitao@gmail.com>
2025-06-24 19:44:27 +0100

    Create MineralMachines.java

---
+++ src/main/java/pt/odnav/mineralmachines/MineralMachines.java
@@ -0,0 +1,20 @@
+package pt.odnav.mineralmachines;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(MineralMachines.MODID)
+public class MineralMachines {
+    public static final String MODID = "mineralmachines";
+    public static final Logger LOGGER = LogManager.getLogger();
+
+    public MineralMachines() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        // TODO: Registar blocos, itens, creative tabs, etc.
+        LOGGER.info("Mineral Machines iniciado!");
+    }
+}
